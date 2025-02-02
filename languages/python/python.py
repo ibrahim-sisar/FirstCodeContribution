fd920fc2a9bf2bddad72ef1e6444052e9fa14d42
ibrahim abu al roos <ibrahim.sisar@gmail.com>
2025-02-02 18:48:18 +0200

    Update python.py

--- languages/python/python.py
+++ languages/python/python.py
@@ -1,1 +1,12 @@
-# test
+def main():
+  # call your function
+  pass
+
+"""
+ You need to write a function that takes an array as input and returns the largest number in that array.
+ Above your function, write a comment with your name and GitHub link.
+ Name your function after your name, considering case sensitivity in the programming language.
+Call your function inside the pre-existing main() function (which is already written in the file).
+"""
+
+# 📢 Good luck and happy coding! 🚀
