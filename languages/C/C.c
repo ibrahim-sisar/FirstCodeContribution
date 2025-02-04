9bd8952ec952c52fa026ca8e3cb47da525d19af2
ibrahim abu al roos <ibrahim.sisar@gmail.com>
2025-02-04 11:15:23 +0200

    Create C.c

---
+++ languages/C/C.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    int arr[] = {10, 45, 3, 67, 23};
+    // call your function
+}
+
+/*
+ You need to write a function that takes an array as input and returns the largest number in that array.
+ Above your function, write a comment with your name and GitHub link.
+ Name your function after your name, considering case sensitivity in the programming language.
+Call your function inside the pre-existing main() function (which is already written in the file).
+*/
+
+// 📢 Good luck and happy coding! 🚀
