7a02a37ed24c4a33dc07a3b62dcef0ab13204480
ibrahim abu al roos <ibrahim.sisar@gmail.com>
2025-02-04 11:02:54 +0200

    Create javascript.js

---
+++ languages/javascript/javascript.js
@@ -0,0 +1,17 @@
+function main() {
+    let arr = [2, 7, 9, 23, 8, 6, 1, 0, -1, 5, 4, 3];
+    // call your function
+}
+
+/*
+ You need to write a function that takes an array as input and returns the largest number in that array.
+ Above your function, write a comment with your name and GitHub link.
+ Name your function after your name, considering case sensitivity in the programming language.
+ Call your function inside the pre-existing main() function (which is already written in the file).
+*/
+
+if (require.main === module) {
+    main();
+}
+
+// 📢 Good luck and happy coding! 🚀
